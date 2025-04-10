0955b3cc235cf6f4e26f65e36544155c6adb15a9
Kai <84909772+kq1o8i4@users.noreply.github.com>
2025-04-10 10:34:53 +0900

    Update BookRepository.java

--- src/main/java/com/example/bookshelf/repository/BookRepository.java
+++ src/main/java/com/example/bookshelf/repository/BookRepository.java
@@ -1,18 +1,14 @@
 package com.example.bookshelf.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.bookshelf.entity.Book;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 	List<Book> findByStatus(String status);
-
-	// タイトル、著者、ステータスが一致するか確認するメソッド
+	
 	boolean existsByTitleAndAuthorAndStatus(String title, String author, String status);
-
-	// レビューが存在する「read」ステータスの本を取得（空文字やnullを除外）
+	
 	List<Book> findByStatusAndReviewIsNotNullAndReviewNot(String status, String empty);
 
 }
